c5d4e9660ed69fd9bc26261c88cf3c4b720363a2
Prernakumari29 <prernajaiswal646@gmail.com>
2025-11-13 16:41:27 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,108 @@
+const images = [
+    "img1.jpg",
+    "img2.jpg",
+    "img3.jpg",
+    "img3.jpg",
+    "img4.jpg",
+    "img5.jpg",
+    "img6.jpg"
+  ];
+  index = 0
+  var imgElement = document.querySelector("#slideImage")
+  var btn = document.querySelector("#dicover-more")
+
+  btn.addEventListener("click" ,function(){
+    index = (index+1) % images.length
+    setTimeout(() => {
+      imgElement.src = images[index];    
+    }, 300);
+  });
+
+
+// --------------------------------------page4--------------------------
+  const hoverImages = document.querySelectorAll('.hoverImage img');
+
+hoverImages.forEach(img => {
+  const originalSrc = img.src;            
+  const hoverSrc = img.dataset.hover;     
+
+
+  img.addEventListener('mouseenter', () => {
+    img.src = hoverSrc;
+  });
+
+
+  img.addEventListener('mouseleave', () => {
+    img.src = originalSrc;
+  });
+});
+
+// ---------------------------------------------annimation------------------------------
+gsap.from("#zara", {
+      y:-30,
+      opacity:0,
+    duration:1,
+    delay:0.5,
+})
+
+var tel = gsap.timeline()
+
+tel.from(".left-right a",{
+  y:-30,
+  opacity:0,
+  duration:1,
+  delay:0.3,
+  stagger:0.3
+})
+
+// tel.from(".section2 h1" , {
+//   opacity:0,
+//   duration:0.8,
+//   scale:0.2,
+//   transformOrigin: "left center"
+// })
+
+// gsap.from(".video video" , {
+//      scale:0,
+//      delay:0.1,
+//      duration:1,
+//      stagger:0.3,
+//      scrollTrigger:{
+//       trigger:".video video",
+//       scroller:"body",
+//      }
+// })
+
+
+gsap.from("#left-text" , {
+     scale:0,
+     opacity:0,
+     delay:0.1,
+     duration:1,
+     stagger:0.3,
+     scrollTrigger:"#left-text"
+})
+
+gsap.to(".video video" , {
+     scale:0.6,
+     delay:0.1,
+     duration:1,
+     stagger:0.3,
+     scrollTrigger:{
+      trigger:".image",
+      scroller:"body",
+      start:"top 70%",
+      end:"top 20%",
+      scrub:2
+     }
+})
+gsap.from(".zarasale img" , {
+    //  scale:0.8,
+     delay:0.6,
+     duration:2,
+     x:-2000,
+     scrollTrigger:{
+      trigger:"#page5",
+      scroller:"body",
+     }
+})
